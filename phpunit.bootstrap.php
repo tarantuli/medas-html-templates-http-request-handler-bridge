e65873432ef652d22df5be25929515ed6fdc76bd
Patrick Morpey <patrick@morphp.nl>
2026-07-29 12:31:11 +0200

    Updated dependencies and upgraded to `medas-core` v4

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -3,14 +3,13 @@
 declare(strict_types=1);
 
 use Medas\HtmlTemplatesHttpRequestHandlerBridge\HtmlTemplatesHttpRequestHandlerBridgePackage;
-use Medas\ObjectInstantiator\ObjectInstantiator;
-use Medas\ObjectInstantiator\ObjectInstantiatorPackage;
-use Medas\ServiceManager\{ServiceConfig, ServiceManager};
+use Medas\ObjectInstantiator\{ObjectInstantiator, ObjectInstantiatorPackage};
+use Medas\ServiceManager\{ServiceConfigBuilder, ServiceManager};
 
 chdir(__DIR__);
 
-new ServiceManager(function (): ServiceConfig {
-    $config = new ServiceConfig(ObjectInstantiator::class);
+new ServiceManager(function (): ServiceConfigBuilder {
+    $config = new ServiceConfigBuilder(ObjectInstantiator::class);
 
     $config->addPackages([
         HtmlTemplatesHttpRequestHandlerBridgePackage::instance(),
